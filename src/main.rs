4ad476e83f012887d2f1bfde6a0776971a3773f2
Russell Currey <ruscur@russell.cc>
2016-02-10 18:05:15 +1100

    Fix copyright headers
    IBM owns copyright on skibot, so update the headers.
    
    Signed-off-by: Russell Currey <ruscur@russell.cc>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2016 Russell Currey <ruscur@russell.cc>
+ * Copyright (c) 2016 IBM Corporation
+ * Author: Russell Currey <ruscur@russell.cc>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
