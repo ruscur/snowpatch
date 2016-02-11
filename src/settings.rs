b46f2270b82c1b3458185e9acde9ae1c0e68fc91
Russell Currey <ruscur@russell.cc>
2016-02-12 09:45:10 +1100

    Update settings.rs heading to the new style
    Signed-off-by: Russell Currey <ruscur@russell.cc>

--- src/settings.rs
+++ src/settings.rs
@@ -1,14 +1,14 @@
-/*
- * Copyright (c) 2016 IBM Corporation
- * Author: Russell Currey <ruscur@russell.cc>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
- */
-
-// settings.rs: code to handle snowpatch settings parsing from TOML
+//
+// Copyright (c) 2016 IBM Corporation
+// Author: Russell Currey <ruscur@russell.cc>
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version.
+//
+// settings.rs - handle snowpatch settings parsing from TOML
+//
 
 use toml;
 use toml::{Parser, Value};
